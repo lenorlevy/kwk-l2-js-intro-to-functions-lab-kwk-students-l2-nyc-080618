8e96c7dbe0cf94747833d189a0b16836fa59b7f3
lenorlevy <lenorlevy1@gmail.com>
2018-08-08 17:30:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code in this file!
 function getUKTime(time) {
-  new Date().getHours() + 5;
-  var return=
+  var result = new Date().getHours() + 5;
+  return result;
 }
 
-getUKTime(ny);
+getUKTime();
